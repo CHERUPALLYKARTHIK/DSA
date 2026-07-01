3e108bcf9a41c69af7e64b959d712e34b37a1624
Cherupally Karthik <147877852+CHERUPALLYKARTHIK@users.noreply.github.com>
2026-07-01 19:21:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,21 +1,9 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        #Bubble Sort
-        """
         n=len(nums)
-        for i in range(n-1):
-            for j in range(n-1-i):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-        return nums"""
-
-        #Dutch National Flag Algorithm
         l=0
         m=0
-        h=len(nums)-1
+        h=n-1
         while m<=h:
             if nums[m]==0:
                 nums[l],nums[m]=nums[m],nums[l]
@@ -26,4 +14,5 @@ class Solution:
             else:
                 nums[m],nums[h]=nums[h],nums[m]
                 h-=1
-
+        return nums
+        
