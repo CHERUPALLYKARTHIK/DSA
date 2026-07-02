771a5434f398fb921a61495b063fb982da74e81f
Cherupally Karthik <147877852+CHERUPALLYKARTHIK@users.noreply.github.com>
2026-07-02 13:36:55 +0530

    Time: 47 ms (61.96%), Space: 43.3 MB (36.54%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,7 +1,7 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         #Brute Force
-        pos=[]
+        '''pos=[]
         neg=[]
         n=len(nums)
         for i in range(n):
@@ -13,6 +13,20 @@ class Solution:
             for i in range(len(pos)):
                 nums[2*i]=pos[i]
                 nums[2*i+1]=neg[i]
-        return nums
+        return nums'''
+        #optimal solution
+        n=len(nums)
+        ans=[0]*n
+        pos=0
+        neg=1
+        for i in range(n):
+            if nums[i]<0:
+                ans[neg]=nums[i]
+                neg+=2
+            else:
+                ans[pos]=nums[i]
+                pos+=2
+        return ans
+
 
         
