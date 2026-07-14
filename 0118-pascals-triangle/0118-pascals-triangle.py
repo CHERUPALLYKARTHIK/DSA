e8699171aaf54feef129e6c00d0fa1be07979f18
Cherupally Karthik <147877852+CHERUPALLYKARTHIK@users.noreply.github.com>
2026-07-14 20:35:26 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.77%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,11 +3,7 @@ class Solution:
         triangle = []
         
         for i in range(numRows):
-            # Create a row filled with 1s, with a length of i + 1
             row = [1] * (i + 1)
-            
-            # Each element (except the first and last) is the sum of
-            # the two elements directly above it in the previous row
             for j in range(1, i):
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
             
