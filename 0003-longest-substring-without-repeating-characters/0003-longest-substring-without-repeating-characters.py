1247d3e156fa532ad5d8d8cb8624e0ef46e661bd
Cherupally Karthik <147877852+CHERUPALLYKARTHIK@users.noreply.github.com>
2026-07-20 22:52:45 +0530

    Time: 10 ms (80.38%), Space: 19.4 MB (7.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -15,4 +15,13 @@ class Solution:
                 uniset.add(ch)
             ans=max(ans,r-l+1)
         return ans
+        #Brute Force
+        '''n=len(s)
+        l=0
+        for i in range(n):
+            for j in range(i+1,n+1):
+                sub=s[i:j]
+                if len(set(sub))==len(sub):
+                    l=max(l,j-i)
+        return l'''
         
